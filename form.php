7dd5db749e68d87cc00ca554d36e594b4d100d66
r.bassomo <r.bassomo@20R2-13.form.willow.if3>
2012-03-16 11:29:59 +0100

    Ajout d'un titre h1

--- form.php
+++ form.php
@@ -5,11 +5,13 @@
         <title></title>
     </head>
     <body>
+        <h1>Formulaire</h1>
         <form method="POST" action="./add.php">
             <fieldset>
                 <label>Nom</label><input type="text" name="name"/>
                 <label>Latitude</label><input type="text" name="latitude"/>
                 <label>Longitude</label><input type="text" name="longitude"/>
+                
                 <input type="submit" value="envoyer"/>
             </fiedset>
             
